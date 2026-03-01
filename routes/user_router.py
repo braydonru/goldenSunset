7a24901f0cd28ba7d2e70c0bfa6a65dd5949db8f
Braydon <braydonru1234@gmail.com>
2026-03-01 14:47:58 -0500

    manage with human resources

--- routes/user_router.py
+++ routes/user_router.py
@@ -23,3 +23,27 @@ def register(db:SessionDep,
     db.commit()
     db.refresh(user_db)
     return "user registered successfully"
+
+
+@user_router.get('/users')
+def get_users(db:SessionDep):
+    response = select(User)
+    users = db.exec(response).all()
+    return users
+
+@user_router.get('/users/{username}')
+def get_user(db:SessionDep,username: str):
+    user = db.query(User).filter(User.username==username).first()
+    if not user:
+        raise HTTPException(status_code=404,detail='User not found',)
+    return user
+
+@user_router.put('/users/{id}')
+def update_user(db:SessionDep,id:int):
+    user = db.get(User,id)
+    if not user:
+        raise HTTPException(status_code=404,detail='User not found')
+    user.is_superuser = True
+    db.commit()
+    db.refresh(user)
+    return user
